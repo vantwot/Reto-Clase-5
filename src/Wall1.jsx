af92c12216aac21029a0a42ad59307605288f52c
Van <salamanca.valentina@correounivalle.edu.co>
2023-04-22 08:19:23 -0500

    Update Wall1.jsx

--- src/Wall1.jsx
+++ src/Wall1.jsx
@@ -1,8 +1,18 @@
+import React, { useRef, useState } from "react";
+import { useVideoTexture } from "@react-three/drei";
+
 export default function Wall1() {
-  
+
+  const [reproducir, setReproducir] = useState(false);
+
+  const handleClick = () => {
+    setReproducir(!reproducir);
+  }
+
   return (
-    <mesh position={[3,1,10]} rotation-y={-Math.PI * 0.8} >
-      <planeGeometry args={[6, 6]} />
+    <mesh position={[3, 1, 10]} rotation-y={-Math.PI * 0.8} onClick={handleClick}>
+      <planeBufferGeometry args={[6, 6]} />
+      <meshBasicMaterial map={new useVideoTexture('1.mp4')}/>
     </mesh>
   );
 }
